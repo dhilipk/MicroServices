2d95612a732fe0ef5379e6a3eabf1c17b10d3229
deswaran <deswaran@paypal.com>
2017-05-30 09:21:48 -0400

    Initial Commit

---
+++ flight-information-service/src/main/java/com/mytrip/flight/information/FlightInformationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.flight.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInformationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInformationServiceApplication.class, args);
+	}
+}
