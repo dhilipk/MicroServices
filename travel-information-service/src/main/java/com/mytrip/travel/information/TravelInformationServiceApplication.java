281ac72ce166fbe85cf0ec15275184cc19ce3ce1
deswaran <deswaran@paypal.com>
2017-05-30 09:49:24 -0400

    Initial Commit

---
+++ travel-information-service/src/main/java/com/mytrip/travel/information/TravelInformationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.travel.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelInformationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelInformationServiceApplication.class, args);
+	}
+}
