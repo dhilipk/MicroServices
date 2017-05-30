9c309f481dce103e890e29e83cc36a8b52cfa1ad
deswaran <deswaran@paypal.com>
2017-05-30 09:38:32 -0400

    Initial Commit

--- flight-search-service/src/main/java/com/mytrip/flight/search/FlightSearchServiceApplication.java
+++ flight-search-service/src/main/java/com/mytrip/flight/search/FlightSearchServiceApplication.java
@@ -1,12 +1,12 @@
-package com.mytrip.flight.information;
+package com.mytrip.flight.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightInformationServiceApplication {
+public class FlightSearchServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightInformationServiceApplication.class, args);
+		SpringApplication.run(FlightSearchServiceApplication.class, args);
 	}
 }
--- flight-search-service/src/test/java/com/mytrip/flight/search/FlightSearchServiceApplicationTests.java
+++ flight-search-service/src/test/java/com/mytrip/flight/search/FlightSearchServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mytrip.flight.information;
+package com.mytrip.flight.search;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FlightInformationServiceApplicationTests {
+public class FlightSearchServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
