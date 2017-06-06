923685246662024533770c4d82e32574850f3119
deswaran <deswaran@paypal.com>
2017-06-06 07:08:36 -0400

    Added Service to retrieve the flight status which has got scheduled.

---
+++ flight-status-service/src/main/java/com/mytrip/flight/status/FlightStatusServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.flight.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightStatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightStatusServiceApplication.class, args);
+	}
+}
