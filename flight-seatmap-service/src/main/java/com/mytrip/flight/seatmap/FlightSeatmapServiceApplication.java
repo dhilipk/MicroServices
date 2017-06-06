f5ba97cab26421e45976504ea64d875b82287fbd
deswaran <deswaran@paypal.com>
2017-06-06 07:39:34 -0400

    Added REST service for flight seat map information and priority seat booking facilities.

---
+++ flight-seatmap-service/src/main/java/com/mytrip/flight/seatmap/FlightSeatmapServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.flight.seatmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSeatmapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSeatmapServiceApplication.class, args);
+	}
+}
---
+++ flight-seatmap-service/src/test/java/com/mytrip/flight/seatmap/FlightSeatmapServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mytrip.flight.seatmap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightSeatmapServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
