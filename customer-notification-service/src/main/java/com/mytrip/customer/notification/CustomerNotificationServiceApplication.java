dc71dece906087caac76a88be03b3befa4978bad
deswaran <deswaran@paypal.com>
2017-06-06 06:20:04 -0400

    Added Customer Notification service to notify customers for any deals / offers / booked information.

---
+++ customer-notification-service/src/main/java/com/mytrip/customer/notification/CustomerNotificationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.customer.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerNotificationServiceApplication.class, args);
+	}
+}
