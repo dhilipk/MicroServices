37d1e6253790a01d61fd529a48867c206596c055
deswaran <deswaran@paypal.com>
2017-06-07 01:38:59 -0400

    Added customer authentication service to validate the logged-in user is already a MyTrip user and continue with the user on the MyTrip Application

---
+++ customer-authentication-service/src/main/java/com/mytrip/customer/authentication/CustomerAuthenticationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.customer.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAuthenticationServiceApplication.class, args);
+	}
+}
