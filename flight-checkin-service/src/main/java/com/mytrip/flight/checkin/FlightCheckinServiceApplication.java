c30a764561c448fa634c1d3a7a0a9355aa43a667
deswaran <deswaran@paypal.com>
2017-06-07 03:48:37 -0400

    Added checkin service to do transaction for the on-boarding of passengers into flight.

---
+++ flight-checkin-service/src/main/java/com/mytrip/flight/checkin/FlightCheckinServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.flight.checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCheckinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightCheckinServiceApplication.class, args);
+	}
+}
