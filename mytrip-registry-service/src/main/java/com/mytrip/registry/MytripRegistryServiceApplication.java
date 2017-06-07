c32d95fc8b198f3d15ae7ef2fcfe2bb2abfd5e42
deswaran <deswaran@paypal.com>
2017-06-07 08:46:24 -0400

    Registry Service for MyTrip application.

---
+++ mytrip-registry-service/src/main/java/com/mytrip/registry/MytripRegistryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mytrip.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MytripRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytripRegistryServiceApplication.class, args);
+	}
+}
---
+++ mytrip-registry-service/src/test/java/com/mytrip/registry/MytripRegistryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mytrip.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MytripRegistryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
