5f966a70738badd59508fa5a4d4450a7c0de389a
deswaran <deswaran@paypal.com>
2017-06-07 06:02:46 -0400

    Added for travel reissue modification for the travel scheduled.

---
+++ travel-reissue-service/src/main/java/com/mytrip/travel/reissue/TravelReissueServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.travel.reissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelReissueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelReissueServiceApplication.class, args);
+	}
+}
---
+++ travel-reissue-service/src/test/java/com/mytrip/travel/reissue/TravelReissueServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mytrip.travel.reissue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelReissueServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
