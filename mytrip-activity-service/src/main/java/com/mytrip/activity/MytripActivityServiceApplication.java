1ed4798033ca9071098a8d2e21bda0b42fb2bdb0
deswaran <deswaran@paypal.com>
2017-06-07 03:49:32 -0400

    MyTrip Activity service will be used to track the user activities which are performed within the application.

---
+++ mytrip-activity-service/src/main/java/com/mytrip/activity/MytripActivityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytripActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytripActivityServiceApplication.class, args);
+	}
+}
