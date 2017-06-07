e33c782a6143ceee18686fcba389884b37886935
deswaran <deswaran@paypal.com>
2017-06-07 06:04:25 -0400

    Added customer disputes service to handle the trouble to resolve for the travel issued.

---
+++ customer-dispute-service/src/main/java/com/mytrip/customer/dispute/CustomerDisputeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.customer.dispute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDisputeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDisputeServiceApplication.class, args);
+	}
+}
---
+++ customer-dispute-service/src/test/java/com/mytrip/customer/dispute/CustomerDisputeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mytrip.customer.dispute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerDisputeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
