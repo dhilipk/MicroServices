356c33c5e1af5d2c3c3cd32d94f667313a3bdc8f
deswaran <deswaran@paypal.com>
2017-06-23 07:27:46 -0400

    Need to add the consul configuration properties

---
+++ mytrip-mobile-gateway/src/main/java/com/mytrip/mobile/gateway/MytripMobileGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.mytrip.mobile.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytripMobileGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytripMobileGatewayApplication.class, args);
+	}
+}
