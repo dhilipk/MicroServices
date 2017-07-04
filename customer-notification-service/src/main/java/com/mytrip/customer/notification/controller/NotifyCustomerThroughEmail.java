2c4861d377b4d76f8ecc7abdc16726c9503a1786
deswaran <deswaran@paypal.com>
2017-07-04 03:15:37 -0400

    Authentication Enabled with Json Web Token.

--- customer-notification-service/src/main/java/com/mytrip/customer/notification/controller/NotifyCustomerThroughEmail.java
+++ customer-notification-service/src/main/java/com/mytrip/customer/notification/controller/NotifyCustomerThroughEmail.java
@@ -1,8 +1,10 @@
 package com.mytrip.customer.notification.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mytrip.common.customer.vo.CustomerAccountVo;
@@ -12,7 +14,8 @@ import com.mytrip.common.customer.vo.CustomerAccountVo;
 @EnableResourceServer
 public class NotifyCustomerThroughEmail {
 
-    @RequestMapping(path = "/notify/{id}")
+    @PreAuthorize(value = "hasRole('R')")
+    @RequestMapping(path = "/notify/{id}", method = RequestMethod.GET)
     public CustomerAccountVo notify(@PathVariable(name = "id", required = true)String id) {
         CustomerAccountVo accountVo = new CustomerAccountVo();
         accountVo.setId(id);
